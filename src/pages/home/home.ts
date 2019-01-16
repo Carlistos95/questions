9112af0684fb1d822d8a00b70f74594482886aad
unknown <carloshoyos26@hotmail.com>
2019-01-16 10:48:46 -0500

    pregunta

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PreguntaImagenPage } from '../pregunta-imagen/pregunta-imagen';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  goPagina2():void{
+    this.navCtrl.push(PreguntaImagenPage);
   }
 
 }
